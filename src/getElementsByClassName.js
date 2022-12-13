a3906b7b9298f6cbacd64655ab223c795e4dc1b0
Terrence Koo <terrencekoo@Terrences-MacBook-Air.local>
2022-12-12 22:36:56 -0500

    Complete Bare Minimum Requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function (className) {
   // your code here
+  var result = [];
+  (function innerFunction (node) {
+    if (node.classList && node.classList.contains(className)) {
+      result.push(node);
+    }
+    for (var i = 0; i < node.children.length; i++) {
+      innerFunction(node.children[i]);
+    }
+  })(document.body);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // your code goes here}
+  if (typeof obj === 'number' || obj === true || obj === false) {
+    return '' + obj;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj) && obj.length === 0) {
+    return '[]';
+  }
+
+  var result = [];
+  if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      if (typeof obj[i] === 'boolean' || typeof obj[i] === 'number') {
+        result.push(obj[i]);
+      } else {
+        result.push(stringifyJSON(obj[i]));
+      }
+    } return '[' + result + ']';
+  }
+
+  if (typeof obj === 'object') {
+    var string = '';
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+        continue;
+      }
+      string += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+    }
+    if (string.length !== 0) {
+      string = string.slice(0, -1);
+    }
+    return '{' + string + '}';
+  }
 };
